6ca7cfecde72627fc31170d27ac1ecbf43e6d6bb
Lee Minhoon <hackids@naver.com>
2021-07-11 04:46:25 +0900

    init commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,20 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  let [글제목, 글제목변경] = useState('남자 코트 추천');
+
   return (
     <div className="App">
       <div className="black-nav">
         <div>개발 Blog</div>
+      </div>
+      <div className="list">
+        <h3> {글제목} </h3>
+        <p>2월 17일 발행</p>
+        <hr />
       </div>
     </div>
   );
